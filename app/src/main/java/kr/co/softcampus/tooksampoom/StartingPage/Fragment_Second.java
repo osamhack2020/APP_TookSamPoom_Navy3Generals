974c965cbea5e42f1b2351c7c4d0eb8338570167
김 민철 <ckqntk12@naver.com>
2020-10-09 07:30:08 +0000

    2

--- app/src/main/java/kr/co/softcampus/tooksampoom/StartingPage/Fragment_Second.java
+++ app/src/main/java/kr/co/softcampus/tooksampoom/StartingPage/Fragment_Second.java
@@ -4,7 +4,23 @@ public class FragSecond extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_second, container, false);
- 
+            
+        ImageButton MaleBtn = (ImageButton)findViewById(R.id.MaleBtn);
+        ImageButton FemaleBtn = (ImageButton)findViewById(R.id.FemaleBtn);
+        
+
+        MaleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+        FemaleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
         return rootView;
         
     }
