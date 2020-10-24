377d15e5f950a0f9e40bb4aafe951027bfdd50e9
minseok4787 <minseok314@naver.com>
2020-10-24 19:08:53 +0900

    Update buttons in fragment

--- app/src/main/java/kr/co/softcampus/tooksampoom/MainFragment.java
+++ app/src/main/java/kr/co/softcampus/tooksampoom/MainFragment.java
@@ -1,5 +1,7 @@
 package kr.co.softcampus.tooksampoom;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +9,94 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MainFragment extends Fragment {
-    ViewGroup viewGroup;
-
+public class MainFragment extends Fragment{
+    View view;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_main,container, false);
+        button1 = (Button) view.findViewById(R.id.button1);
+        button2 = (Button) view.findViewById(R.id.button2);
+        button3 = (Button) view.findViewById(R.id.button3);
+        button4 = (Button) view.findViewById(R.id.button4);
+        button5 = (Button) view.findViewById(R.id.button5);
+        button6 = (Button) view.findViewById(R.id.button6);
 
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return view;
+    }
+
+    public void onClickCCA(View view) {
+        Intent ccaIntent = new Intent(getActivity(), CameraCaptureActivity.class);
+        startActivity(ccaIntent);
+    }
+
+    public void onClickPushUpBt(View view) {
+        Intent puIntent = new Intent(getActivity(), PushUpMeasureActivity.class);
+        startActivity(puIntent);
+    }
+
+    public void onClickRunning(View view) {
+        Intent runningIntent = new Intent(getActivity(), RunningActivity.class);
+        startActivityForResult(runningIntent, 0);
+    }
+
+    public void onClickRecord(View view) {
+        Intent recordIntent = new Intent(getActivity(), GraphActivity.class);
+        startActivityForResult(recordIntent, 0);
+    }
+
+    public void onClickDummy(View view){
+        for(int j=1; j<5; j++){
+            for(int i=10; i<24; i++){
+                RecordInfo recordinfo = new RecordInfo();
+                recordinfo.setId(j);
+                recordinfo.setPushup(67+i+10*j);
+                recordinfo.setRunning(720+i+10*j);
+                recordinfo.setSitup(80+i+10*j);
+
+                DBhelper helper = new DBhelper(getActivity());
+                SQLiteDatabase db = helper.getWritableDatabase();
+
+                String sql1 = "INSERT INTO Record_Push_Up(id,"
+                        +"push_up,"
+                        +"date) VALUES(?,?,?)";
+                String date1 = "2020-10-"+i+" 20:10:43";
+                String[] value1 = {Integer.toString(j),
+                        Integer.toString(recordinfo.push_up),
+                        date1};
+                db.execSQL(sql1,value1);
+
+                String sql2 = "INSERT INTO Record_Sit_Up(id,"
+                        +"sit_up,"
+                        +"date) VALUES(?,?,?)";
+                String date2 = "2020-10-"+i+" 20:10:43";
+                String[] value2 = {Integer.toString(j),
+                        Integer.toString(recordinfo.sit_up),
+                        date2};
+                db.execSQL(sql2,value2);
+
+                String sql3 = "INSERT INTO Record_Running(id,"
+                        +"running,"
+                        +"date) VALUES(?,?,?)";
+                String date3 = "2020-10-"+i+" 20:10:43";
+                String[] value3 = {Integer.toString(j),
+                        Integer.toString(recordinfo.running),
+                        date3};
+                db.execSQL(sql3,value3);
+
+                db.close();
+            }
+        }
     }
 }
