75d68c29777d5bf38f02250686022896477fc1ff
minseok4787 <minseok314@naver.com>
2020-10-25 22:16:41 +0900

    Update radar chart

--- app/src/main/java/kr/co/softcampus/tooksampoom/MainFragment.java
+++ app/src/main/java/kr/co/softcampus/tooksampoom/MainFragment.java
@@ -50,35 +50,49 @@ public class MainFragment extends Fragment{
 
 
         ArrayList<RadarEntry> dataValue = new ArrayList<>();
-        float a = (float)(46.0/76.0)*100;
-        dataValue.add(new RadarEntry(a));
-        dataValue.add(new RadarEntry((float)(54.0/86.0)*100));
-        dataValue.add(new RadarEntry((float)(623.0/760.0)*100));
+        dataValue.add(new RadarEntry(formatPushUp(120)));
+        dataValue.add(new RadarEntry(formatSitUp(80)));
+        dataValue.add(new RadarEntry(formatRunning(1300)));
 
         RadarDataSet dataSet = new RadarDataSet(dataValue, "data");
         dataSet.setColor(Color.BLUE);
         dataSet.setColor(R.color.colorPrimary);
         dataSet.setDrawFilled(true);
-        dataSet.setValueTextSize(13f);
+        dataSet.setValueTextSize(10f);
         RadarData data = new RadarData();
-        String[] values = {46+"개", 54+"개", 623/60+"분 "+623%60+"초"};
+        String[] values = {120+"개", 80+"개", 1300/60+"분 "+1300%60+"초"};
         dataSet.setValueFormatter(new MyValueFormatter(values));
         data.addDataSet(dataSet);
 
         String[] labels =  {"팔굽혀펴기", "윗몸일으키기", "3km 달리기"};
         XAxis xAxis = radarChart.getXAxis();
-        xAxis.setTextSize(13.5f);
+        xAxis.setTextSize(11f);
         xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
 
         YAxis yAxis = radarChart.getYAxis();
-        yAxis.setAxisMinimum(0f);
-        yAxis.setAxisMaximum(100f);
-        yAxis.setLabelCount(5,true);
-        yAxis.setTextSize(11f);
+        yAxis.setAxisMinimum(0);
+        yAxis.setAxisMaximum(5);
+        yAxis.setLabelCount(6,true);
+        yAxis.setTextSize(9f);
         yAxis.setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
-                if(value==100){
+                if(value==0){
+                    return "";
+                }
+                if(value==1){
+                    return "미달";
+                }
+                if(value==2){
+                    return "3급";
+                }
+                if(value==3){
+                    return "2급";
+                }
+                if(value==4){
+                    return "1급";
+                }
+                if(value==5){
                     return  "특급";
                 }
                 else{
@@ -92,6 +106,73 @@ public class MainFragment extends Fragment{
 
 
         return view;
+    }
+
+    public float formatPushUp(int value){
+        if(value>100){
+            return 5;
+        }
+        if(value<=47){
+            return (float)value/47;
+        }
+        else if(value>=48 && value<=55){
+            return 1+(float)(value-48)/(55-48);
+        }
+        else if(value>=56 && value<=63){
+            return 2+(float)(value-56)/(63-56);
+        }
+        else if(value>=64 && value<=71){
+            return 3+(float)(value-64)/(71-64);
+        }
+        else{
+            return 4+(float)(value-72)/(100-72);
+        }
+    }
+
+    public float formatSitUp(int value){
+        if(value>100){
+            return 5;
+        }
+        if(value<=61){
+            return (float)value/61;
+        }
+        else if(value>=62 && value<=69){
+            return 1+(float)(value-62)/(69-62);
+        }
+        else if(value>=70 && value<=77){
+            return 2+(float)(value-70)/(77-70);
+        }
+        else if(value>=78 && value<=85){
+            return 3+(float)(value-78)/(85-78);
+        }
+        else{
+            return 4+(float)(value-86)/(100-86);
+        }
+    }
+
+    public float formatRunning(int value){
+        int mValue = value*-1;
+        if(mValue>-600){
+            return 5;
+        }
+        if(mValue<-1200){
+            return 0;
+        }
+        if(mValue<=-937){
+            return (float)mValue-(-1200)/(-937-(-1200));
+        }
+        else if(mValue>=-936 && mValue<=-875){
+            return 1+(float)(mValue-(-936))/(-875-(-936));
+        }
+        else if(mValue>=-874 && mValue<=-813){
+            return 2+(float)(mValue-(-874))/(-813-(-874));
+        }
+        else if(mValue>=-812 && mValue<=-751){
+            return 3+(float)(mValue-(-812))/(-751-(-812));
+        }
+        else{
+            return 4+(float)(mValue-(-750))/(-600-(-750));
+        }
     }
 
     public void onClickCCA(View view) {
